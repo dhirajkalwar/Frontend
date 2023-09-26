fb3bffc1fb55cc7bc6287062c14d3ebd63478031
Amxn-2 <amansharmaa1205@gmail.com>
2023-09-26 16:03:04 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios'],
-    }
-  }
-})
+  plugins: [react()]
+  // ,
+  // build: {
+  //   rollupOptions: {
+  //     external: ['axios'],
+  //   }
+  // }
+}
+)
